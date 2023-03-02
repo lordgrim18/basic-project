7252969c1d160bc60e6f6fe35ca60d5035a56bf2
“Jerom <darkknightjerom@gmail.com>
2023-03-03 00:02:05 +0530

    customization

---
+++ 2.hangman/hangman.py
@@ -0,0 +1,68 @@
+import random
+from words import words
+from hangman_visual import lives_visual_dict
+import string
+
+#getting word from word collection
+def get_valid_word(words):
+    word = random.choice(words)  # randomly chooses something from the list
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+
+    return word.upper()
+
+#game
+def hangman():
+    word = get_valid_word(words)
+    word_letters = set(word)  # letters in the word
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set()  # what the user has guessed
+
+    lives = 0
+
+    # getting user input
+    while len(word_letters) > 0 and lives < 9 :
+        # letters used
+        # ' '.join(['a', 'b', 'cd']) --> 'a b cd'
+        print('You have', lives, 'lives left and you have used these letters: ', ' '.join(used_letters))
+
+        # what current word is (ie W - R D)
+        word_list = [letter if letter in used_letters else '-' for letter in word]
+        print(lives_visual_dict[lives])
+        print('Current word: ', ' '.join(word_list))
+
+        user_letter = input('Guess a letter: ').upper()
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+                print('')
+
+            else:
+                lives = lives + 1  # takes away a life if wrong
+                print('\nYour letter,', user_letter, 'is not in the word.')
+
+        elif user_letter in used_letters:
+            print('\nYou have already used that letter. Guess another letter.')
+
+        else:
+            print('\nThat is not a valid letter.')
+
+    # gets here when len(word_letters) == 0 OR when lives == 0
+    if lives == 9:
+        print(lives_visual_dict[lives])
+        print('You died, sorry. The word was', word)
+    else:
+        print('YAY! You guessed the word', word, '!!')
+
+
+#main
+while True:
+    ch   = input("Do you want to play the game(Yes/No): ").lower()
+    if ch == "yes":
+        hangman()
+    elif ch == 'no':
+        print("Thank you")
+        quit()
+    else:
+        print("Invalid input, enter yes or no")
